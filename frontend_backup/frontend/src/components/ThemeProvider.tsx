85dd5b60f4c51327afec1147a0db807b4d7af7c8
Nitesh Kumar Das <niteshkumardas2025@gmail.com>
2025-07-13 21:43:56 +0530

    Remove frontend_backup folder from repository

--- frontend_backup/frontend/src/components/ThemeProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // Theme system removed - using fixed light theme
-  return <>{children}</>;
-};
-
-export default ThemeProvider;
